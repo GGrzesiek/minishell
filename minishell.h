61740b4311f22900cec870ec75d9e01209b0c22f
GGrzesiek <kryszczuk.grzegorz_4a@op.pl>
2025-12-09 14:38:51 +0100

    feat: added libraries for all allowed func

---
+++ minishell.h
@@ -0,0 +1,23 @@
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <dirent.h>
+#include <string.h>
+#include <termios.h>
+#include <curses.h>
+#include <term.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
