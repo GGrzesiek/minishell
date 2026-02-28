fdeb992fd866c75c8416c825c0dde703ceaf6653
kochamkotkiic <01197869@pw.edu.pl>
2026-02-28 16:22:29 +0100

    fixed norm a bit

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: emilka <emilka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 12:34:40 by sandrzej          #+#    #+#             */
-/*   Updated: 2026/02/28 15:33:49 by emilka           ###   ########.fr       */
+/*   Updated: 2026/02/28 16:15:33 by emilka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_cmd	*parse_tokens(t_shell *shell, t_token *tokens)
 	t_cmd	*curr;
 	t_token	*tmp;
 
-	// NOWOŚĆ: Najpierw przechodzimy przez tokeny i podmieniamy ich tekst
+	// zmiana: najpierw przechodzimy przez tokeny i podmieniamy ich tekst
 	// na wersję rozwiniętą (bez cudzysłowów, z podmienionym $USER itd.)
 	tmp = tokens;
 	while (tmp)
@@ -32,13 +32,12 @@ t_cmd	*parse_tokens(t_shell *shell, t_token *tokens)
 		if (tmp->type == TOKEN_WORD)
 		{
 			char *expanded = expand_token(shell, tmp->value);
-			free(tmp->value); // Zwalniamy starą, "brudną" wersję z Lexera
-			tmp->value = expanded; // Podmieniamy na nową, czystą
+			free(tmp->value); 
+			tmp->value = expanded;
 		}
 		tmp = tmp->next;
 	}
 
-	// Reszta kodu zostaje w 100% tak, jak mieliście oryginalnie!
 	head = NULL;
 	curr = init_cmd();
 	if (!curr)
@@ -59,7 +58,6 @@ t_cmd	*parse_tokens(t_shell *shell, t_token *tokens)
 		{
 			if (tmp->next && tmp->next->type == TOKEN_WORD)
 			{
-				// Tutaj tmp->next->value jest JUŻ rozwinięte przez pętlę wyżej!
 				redir_add_back(&curr->redirs, new_redir(tmp->type,
 						tmp->next->value));
 				tmp = tmp->next;
@@ -69,7 +67,6 @@ t_cmd	*parse_tokens(t_shell *shell, t_token *tokens)
 		}
 		else if (tmp->type == TOKEN_WORD)
 		{
-			// Tutaj tmp->value też jest JUŻ rozwinięte!
 			add_arg(curr, tmp->value);
 		}
 		tmp = tmp->next;
--- parser_utils2.c
+++ parser_utils2.c
@@ -6,12 +6,12 @@
 /*   By: emilka <emilka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 14:57:09 by emilka            #+#    #+#             */
-/*   Updated: 2026/02/28 15:00:27 by emilka           ###   ########.fr       */
+/*   Updated: 2026/02/28 16:20:19 by emilka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-// Dokleja jeden znak do stringa i zwalnia stary string
+
 char	*append_char(char *str, char c)
 {
 	char	*new_str;
@@ -34,7 +34,6 @@ char	*append_char(char *str, char c)
 	return (new_str);
 }
 
-// Dokleja drugi string do pierwszego i zwalnia pierwszy
 char	*append_str(char *str, char *append)
 {
 	char	*new_str;
@@ -49,6 +48,32 @@ char	*append_str(char *str, char *append)
 	free(str);
 	return (new_str);
 }
+static char	*handle_dollar(t_shell *shell, char *str, int *i, char *res)
+{
+	int		start;
+	char	*tmp;
+
+	(*i)++;
+	if (str[*i] == '?')
+	{
+		tmp = ft_itoa(shell->exit_code);
+		res = append_str(res, tmp);
+		(*i)++;
+	}
+	else if (ft_isalpha(str[*i]) || str[*i] == '_')
+	{
+		start = *i;
+		while (ft_isalnum(str[*i]) || str[*i] == '_')
+			(*i)++;
+		tmp = ft_substr(str, start, *i - start);
+		res = append_str(res, env_get(&shell->env_list, tmp));
+	}
+	else
+		return (append_char(res, '$'));
+	free(tmp);
+	return (res);
+}
+
 char	*expand_token(t_shell *shell, char *str)
 {
 	char	*res = ft_strdup("");
@@ -58,51 +83,20 @@ char	*expand_token(t_shell *shell, char *str)
 
 	while (str[i])
 	{
-		// 1. Obsługa cudzysłowów pojedynczych '
 		if (str[i] == '\'' && !in_dq)
 		{
-			in_sq = !in_sq; // włącz/wyłącz
+			in_sq = !in_sq;
 			i++;
 		}
-		// 2. Obsługa cudzysłowów podwójnych "
 		else if (str[i] == '\"' && !in_sq)
 		{
-			in_dq = !in_dq; // włącz/wyłącz
+			in_dq = !in_dq;
 			i++;
 		}
-		// 3. Obsługa znaku dolara $ (tylko poza pojedynczymi cudzysłowami)
 		else if (str[i] == '$' && !in_sq)
-		{
-			i++;
-			if (str[i] == '?') // Zmienna $?
-			{
-				char *num = ft_itoa(shell->exit_code);
-				res = append_str(res, num);
-				free(num);
-				i++;
-			}
-			else if (ft_isalpha(str[i]) || str[i] == '_') // Zwykła zmienna np. $USER
-			{
-				int start = i;
-				while (ft_isalnum(str[i]) || str[i] == '_')
-					i++;
-				char *var_name = ft_substr(str, start, i - start);
-				char *var_val = env_get(&shell->env_list, var_name);
-				if (var_val)
-					res = append_str(res, var_val); // doklej wartość jeśli istnieje
-				free(var_name);
-			}
-			else // Zwykły znak $, po którym nic nie ma
-			{
-				res = append_char(res, '$');
-			}
-		}
-		// 4. Każdy inny, zwykły znak
+			res = handle_dollar(shell, str, &i, res);
 		else
-		{
-			res = append_char(res, str[i]);
-			i++;
-		}
+			res = append_char(res, str[i++]);
 	}
 	return (res);
 }
